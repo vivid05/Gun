12bac156a523b2919932d9e4c4ea0a0c870cd35c
wangxj <wangxj@psds.com.cn>
2019-10-11 13:59:37 +0800

    头部组件渲染

---
+++ src/header/header.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import  './header.css'
+class header extends Component {
+    render() {
+        return (
+            <div className="header">
+                <div className="layout">
+                    <a href="#" className="fl">
+                        <img src="./images/logo.png" alt=""/>
+                    </a>
+                    <a href="#" className="fl TextLink">前往旧版</a>
+                    <a href="#" className="fr login">
+                        <img src="./images/login.svg" alt=""/>
+                        <span>请通过Steam登录</span>
+                    </a>
+                </div>
+            </div>
+        );
+    }
+}
+export default header;
